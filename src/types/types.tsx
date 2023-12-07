febda1312b0b7d627fbcc6c538f8fb14d5a6b135
victor.castor <victor.castor@shopee.com>
2023-12-07 20:58:49 +0700

    feat: add interface of types

---
+++ src/types/types.tsx
@@ -0,0 +1,40 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  password: string;
+  membership?: string;
+  balance?: number;
+  bookmarks?: [];
+}
+
+export interface Merch {
+  id: number;
+  stock: number;
+  price: number;
+  name: string;
+  image: string[];
+  eventId: number;
+}
+
+export interface Purchase {
+  id: number;
+  userId: number;
+  eventId: number;
+  purchaseDate: string;
+  merchList: Merch[];
+  paymentStatus: boolean;
+}
+
+export interface Event {
+  id: number;
+  name: string;
+  startTime: string;
+  duration: number;
+  category: string;
+  price: number;
+  image: string[];
+  description: string;
+  capacity: number;
+}
