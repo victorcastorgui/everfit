88564b45f83476db149540126868809bac38f0a8
victor.castor <victor.castor@shopee.com>
2023-12-08 15:53:34 +0700

    feat: add different display when logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 import { getCookie } from "@/utils/CheckCookie";
 import Link from "next/link";
-import { useRef } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 
 function Navbar() {
-  let cookieExists = "";
-  useRef(() => {
-    cookieExists = getCookie("token");
-  });
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const redirectProfile = () => {
+    router.push("/profile");
+  };
+  useEffect(() => {
+    const cookieExists = getCookie("token");
+    if (cookieExists != "") {
+      setIsLoggedIn(true);
+    }
+  }, []);
   return (
     <div className="bg-black py-3">
       <div className="flex justify-between w-[85%] m-auto">
@@ -16,18 +24,22 @@ function Navbar() {
         <ul className="flex gap-[2.5rem] items-center text-white">
           <Link href="/home">Home</Link>
           <Link href="/events">Events</Link>
-          {cookieExists != "" ? (
-            <>
-              <button>Login</button>
-              <button>Sign Up</button>
-            </>
-          ) : (
+          {isLoggedIn ? (
             <>
               <Link href="/bookmarks">Bookmarks</Link>
               <Link href="/purchasehistory">Purchase History</Link>
-              <div>
+              <div onClick={redirectProfile}>
                 <img src="/images/defaultAvatar.png" alt="avatar" />
               </div>
+            </>
+          ) : (
+            <>
+              <button className="bg-white text-black py-[0.2rem] px-[0.3rem] rounded-[0.5rem]">
+                Sign Up
+              </button>
+              <button className="text-white py-[0.2rem] px-[0.6rem] border-white border-[1px] rounded-[0.5rem]">
+                Login
+              </button>
             </>
           )}
         </ul>
