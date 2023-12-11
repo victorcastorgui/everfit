5a54a65a3e86bebf1d8587bbf4ad0d807bd3cb48
victor.castor <victor.castor@shopee.com>
2023-12-11 10:53:09 +0700

    feat: add initial membership top up

---
+++ src/components/ChangeMembership.tsx
@@ -0,0 +1,51 @@
+import { User } from "@/types/types";
+import Image from "next/image";
+import { SetStateAction, useState } from "react";
+
+function ChangeMembership({
+  setShowMemberModal,
+  data,
+}: {
+  setShowMemberModal: React.Dispatch<SetStateAction<boolean>>;
+  data: User;
+}) {
+  const [success, setSuccess] = useState(false);
+  const handleCloseModal = () => {
+    setSuccess(!success);
+    setShowMemberModal(false);
+  };
+  return (
+    <div
+      onClick={() => setShowMemberModal(false)}
+      className="inset-0 w-screen h-screen bg-[#00000040] z-10 absolute flex justify-center items-center"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="p-[2rem] w-[50%] m-auto bg-white justify-between rounded-[0.5rem]"
+      >
+        {success ? (
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              src="/images/success.png"
+              width={300}
+              height={300}
+              alt="success image"
+            />
+            <h3>Top Up Successful!</h3>
+            <button onClick={handleCloseModal}>Click to close!</button>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <p>Silver</p>
+            </div>
+            <div>Gold</div>
+            <div>Platinum</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ChangeMembership;
