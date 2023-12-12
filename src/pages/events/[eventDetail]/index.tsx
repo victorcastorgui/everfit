2a4aaffc7e7b292f13b50b35d966f25c6a949051
victor.castor <victor.castor@shopee.com>
2023-12-12 15:56:53 +0700

    feat: add event detail page

---
+++ src/pages/events/[eventDetail]/index.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @next/next/no-img-element */
+import PageTitle from "@/components/PageTitle";
+import { API_URL } from "@/utils/API_URL";
+import { IDRFormat } from "@/utils/IDRFormat";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+function DetailPage() {
+  const { push, query } = useRouter();
+  const eventId = query.eventDetail;
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data } = useSWR(`${API_URL}/events/${eventId}`, fetcher);
+  const handleBackPage = () => {
+    push("/events");
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between items-center w-[85%] m-auto">
+        <p onClick={handleBackPage} className="mt-[2rem]">
+          {"< Back"}
+        </p>
+        <PageTitle>{data?.name}</PageTitle>
+        <br />
+      </div>
+      <div className="bg-black rounded-[0.5rem] mt-[2rem] w-[85%] m-auto p-[1rem]">
+        <div className="w-[80%] m-auto text-white">
+          <div className="flex justify-between">
+            <img
+              className="rounded-[0.5rem] w-[50%]"
+              src={data?.image}
+              alt="event image"
+            />
+            <div className="text-[1.5rem]">
+              <p>Start time: {data?.startTime}</p>
+              <p>Duration: {data?.duration}</p>
+              <p>Category: {data?.category}</p>
+              <p>Price: {IDRFormat.format(data?.price)}</p>
+              <p>Capacity: {data?.capacity}</p>
+            </div>
+          </div>
+          <p className="mt-[1rem] text-[1.5rem]">{data?.description}</p>
+        </div>
+      </div>
+      <div className="flex w-[85%] m-auto justify-end mt-[2rem]">
+        <button className="bg-black rounded-[0.5rem] p-[0.5rem] text-white">
+          Buy Ticket
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DetailPage;
