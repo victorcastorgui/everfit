7e1d3d73603a70e1d927a0d9232cdb30e8477534
victor.castor <victor.castor@shopee.com>
2023-12-12 15:56:25 +0700

    feat: add button bookemark, pass userid, detail

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
+import BookmarkButton from "@/components/BookmarkButton";
 import PageTitle from "@/components/PageTitle";
 import { Event } from "@/types/types";
 import { API_URL } from "@/utils/API_URL";
 import { IDRFormat } from "@/utils/IDRFormat";
+import Cookie from "js-cookie";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
-function Event() {
+function Events() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  const { data } = useSWR(`${API_URL}/events`, fetcher);
+  const { data } = useSWR<Event[]>(`${API_URL}/events`, fetcher);
   const router = useRouter();
+  const userId = Cookie.get("id");
 
   const handleDetail = (id: number) => {
     router.push(`/events/${id}`);
@@ -20,27 +23,28 @@ function Event() {
       <div className="text-center">
         <PageTitle>Events</PageTitle>
       </div>
-      <div className="w-[85%] m-auto mt-[2rem] grid grid-cols-4 gap-x-[2rem] gap-y-[3rem]">
-        {data?.map((item: any) => (
+      <div className="w-[85%] m-auto mt-[2rem] grid grid-cols-4 gap-x-[2rem] gap-y-[3rem] ">
+        {data?.map((item) => (
           <div
-            onClick={() => {
-              handleDetail(item.id);
-            }}
-            className="bg-black border-[3px] border-black rounded-[0.5rem] text-white w-[100%] text-center hover:border-[3px] hover:border-gray-500"
-            key={item.key}
+            className="bg-black border-[3px] border-black rounded-[0.5rem] text-white w-[100%] text-center hover:border-[3px] hover:border-white hover:shadow-2xl"
+            key={item.id}
           >
-            <div className="h-[15rem] rounded-[0.5rem] object-contain">
+            <div className="h-[15rem] rounded-[0.5rem] object-contain relative">
               <img
                 className="rounded-[0.5rem] h-[100%] w-[100%]"
                 src={item.image}
                 alt="event image"
               />
+              <div onClick={(e) => e.stopPropagation}>
+                <BookmarkButton eventId={item.id} userId={userId as string} />
+              </div>
             </div>
             <div className="w-[90%] m-auto">
               <h2 className="text-[1.5rem] mt-[1rem]">{item.name}</h2>
               <p className="mt-[0.5rem]">{item.startTime}</p>
               <p className="mt-[0.5rem]">{IDRFormat.format(item.price)}</p>
-              <p className="my-[0.5rem]">{item.description}</p>
+              <p className="my-[0.5rem] truncate">{item.description}</p>
+              <button onClick={() => handleDetail(item.id)}>Detail</button>
             </div>
           </div>
         ))}
@@ -49,4 +53,4 @@ function Event() {
   );
 }
 
-export default Event;
+export default Events;
