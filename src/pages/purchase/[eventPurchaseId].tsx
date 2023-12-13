1bcdaa0f506fffaf0680c122f47d6b5e18379b61
victor.castor <victor.castor@shopee.com>
2023-12-13 13:12:08 +0700

    feat: import stepper, controller, and pages

--- src/pages/purchase/[eventPurchaseId].tsx
+++ src/pages/purchase/[eventPurchaseId].tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import Complete from "@/components/Complete";
+import Merchandise from "@/components/Merchandise";
+import Payment from "@/components/Payment";
+import Stepper from "@/components/Stepper";
+import StepperController from "@/components/StepperController";
+import { useState } from "react";
 
 function EventPurchaseId() {
-  return <div>1</div>;
+  const [currentStep, setCurrentStep] = useState(1);
+  const displayStep = (step: number) => {
+    switch (step) {
+      case 1:
+        return <Merchandise />;
+      case 2:
+        return <Payment />;
+      case 3:
+        return <Complete />;
+      default:
+    }
+  };
+  return (
+    <div className="flex flex-col items-center mt-8">
+      <Stepper currentStep={currentStep} />
+      <div>{displayStep(currentStep)}</div>
+      <StepperController
+        currentStep={currentStep}
+        setCurrentStep={setCurrentStep}
+      />
+    </div>
+  );
 }
 
 export default EventPurchaseId;
