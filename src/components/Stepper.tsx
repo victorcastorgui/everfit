ba26204872420d055a498e68f4e96d956943147c
victor.castor <victor.castor@shopee.com>
2023-12-13 13:10:15 +0700

    feat: add conditions for stepper

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,9 +1,17 @@
-function Stepper() {
+function Stepper({ currentStep }: { currentStep: number }) {
   return (
     <ul className="steps">
       <li className="step step-neutral">Merchandise</li>
-      <li className="step">Payment Method</li>
-      <li className="step">Complete</li>
+      {currentStep >= 2 ? (
+        <li className="step step-neutral">Payment Method</li>
+      ) : (
+        <li className="step">Payment Method</li>
+      )}
+      {currentStep === 3 ? (
+        <li className="step step-neutral">Complete</li>
+      ) : (
+        <li className="step">Complete</li>
+      )}
     </ul>
   );
 }
