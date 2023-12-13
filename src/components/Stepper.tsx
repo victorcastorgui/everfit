b66bf9d6115f5635c0e0b42b076d5ccac03642c3
victor.castor <victor.castor@shopee.com>
2023-12-13 12:48:07 +0700

    feat: add stepper from daisy ui

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,11 @@
+function Stepper() {
+  return (
+    <ul className="steps">
+      <li className="step step-neutral">Merchandise</li>
+      <li className="step">Payment Method</li>
+      <li className="step">Complete</li>
+    </ul>
+  );
+}
+
+export default Stepper;
