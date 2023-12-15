a1dea2e6c126b2c99c7d7f84b8bd10ad1dde4e12
victor.castor <victor.castor@shopee.com>
2023-12-15 13:45:21 +0700

    feat: responsive design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,12 @@ import Logo from "./Logo";
 
 function Footer() {
   return (
-    <div className="bg-black h-[15rem] mt-[2rem]">
+    <div className="bg-black h-[15rem] mt-[2rem] max-[800px]:h-[18rem]">
       <div className="flex flex-col justify-center gap-[2rem] w-[85%] h-[100%] m-auto text-white">
         <Logo />
-        <div className="flex justify-between">
+        <div className="flex justify-between max-[800px]:flex-col">
           <p>Copyright 2023 &copy; EVERFIT</p>
-          <ul className="flex gap-[1rem]">
+          <ul className="flex gap-[1rem] max-[800px]:flex-col">
             <li>Privacy Policy</li>
             <li>Terms & Conditions</li>
             <li>Cookie Policy</li>
