1e78a1adafe159103fa1f3458a74b73d8df5d607
victor.castor <victor.castor@shopee.com>
2023-12-15 17:08:43 +0700

    feat: add responsive styling

--- src/pages/history/[historyDetail]/index.tsx
+++ src/pages/history/[historyDetail]/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import BackPage from "@/components/BackPage";
 import PageTitle from "@/components/PageTitle";
 import { Event, Merch } from "@/types/types";
@@ -40,9 +41,9 @@ function HistoryDetail() {
       {data?.map((item) => (
         <div
           key={item.id}
-          className="grid grid-cols-3 bg-black rounded-[0.5rem] w-[85%] m-auto mt-[2rem] text-white mb-[2rem] p-[1rem]"
+          className="max-[800px]:grid-cols-1 grid grid-cols-3 bg-black rounded-[0.5rem] w-[85%] m-auto mt-[2rem] text-white mb-[2rem] p-[1rem] gap-[1rem] items-center"
         >
-          <div className="overflow-hidden object-cover rounded-[0.5rem] h-[11rem] w-[19rem]">
+          <div className="overflow-hidden object-cover rounded-[0.5rem] max-[800px]:h-[100%] h-[11rem] w-[100%]">
             <img src={item.event.image} alt="event image" />
           </div>
           <div>
@@ -61,7 +62,7 @@ function HistoryDetail() {
               </div>
             ))}
             <p>Discount: {item.discount}%</p>
-            <p>
+            <p className="text-end">
               Total Price:{" "}
               <span className="text-[1.5rem]">
                 {IDRFormat.format(item.paymentTotal)}
