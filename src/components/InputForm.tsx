3c37c2473fc8a0810ebe3441b634ffa04c3faaeb
victor.castor <victor.castor@shopee.com>
2023-12-20 16:29:50 +0700

    feat: add radius

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -3,7 +3,10 @@ import { InputHTMLAttributes } from "react";
 function InputForm(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <>
-      <input className="text-black bg-white w-full p-4" {...props} />
+      <input
+        className="text-black bg-white w-full p-4 rounded-[0.5rem]"
+        {...props}
+      />
     </>
   );
 }
