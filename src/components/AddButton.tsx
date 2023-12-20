4c6fa2ddae76651d7509b793f5f73fe3d621ff53
victor.castor <victor.castor@shopee.com>
2023-12-20 20:37:00 +0700

    fix: styling

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -8,14 +8,12 @@ function AddButton({
   handleFunction: () => void;
 }) {
   return (
-    <div className="w-[90%] flex justify-end">
-      <button
-        onClick={handleFunction}
-        className="flex justify-center items-center border-[2px] bg-black text-white hover:bg-white border-black hover:text-black rounded-[0.5rem] w-24 h-10"
-      >
-        {children}
-      </button>
-    </div>
+    <button
+      onClick={handleFunction}
+      className="flex justify-center items-center border-[2px] bg-black text-white hover:bg-white border-black hover:text-black rounded-[0.5rem] w-24 h-10"
+    >
+      {children}
+    </button>
   );
 }
 
