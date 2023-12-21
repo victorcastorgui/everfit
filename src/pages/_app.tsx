b8079d779fb93f6f63e873f7faf88c879c312c79
victor.castor <victor.castor@shopee.com>
2023-12-21 21:36:47 +0700

    remove: provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,15 @@
-import { store } from "@/stores/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import Layout from "./layout";
 import { Toaster } from "sonner";
+import Layout from "./layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <>
       <Toaster richColors position="top-center" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </Provider>
+    </>
   );
 }
